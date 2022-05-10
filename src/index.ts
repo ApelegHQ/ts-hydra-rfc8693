bc6efc12e68a29406b5a250ee35294e6ff3bc499
Ricardo Iván Vieitez Parra <ricardo@exactrealty.co.uk>
2022-05-11 01:07:46 +0200

    Updated licence information

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 /* Copyright © 2021 Exact Realty Limited.
  *
- * All rights reserved.
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
  * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
